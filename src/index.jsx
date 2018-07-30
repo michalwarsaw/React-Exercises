c203058ae879826cb01189cd0bb8dee0166cd112
Michal Wlodarczyk <polskito@gmail.com>
2018-07-30 14:52:01 +0200

    Second Commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../exercises/ex1';
+import App from '../exercises/ex2.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
