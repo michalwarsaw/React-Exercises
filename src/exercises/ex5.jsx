6ff48bb2d9ebcdaba00460a612c653db991c085d
Michal Wlodarczyk <polskito@gmail.com>
2018-08-05 21:46:51 +0200

    Update

---
+++ src/exercises/ex5.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class New extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            button: "Click Me",
+            text: ""
+        }
+    }
+    handleStartClick = (e) => {
+        console.log(e);
+    }
+    onChangeInput = (e) => {
+        this.setState({ text: e.target.value })
+    }
+    render() {
+        return (
+                <div>
+                    <button onClick={this.handleStartClick}> {this.state.button} </button>
+                    <br />
+                    <input onChange={this.onChangeInput} />
+                    <br />
+                    <h6> Text from input: {this.state.text} </h6>
+                </div>
+                )
+    }
+}
+
+export default class App extends React.Component {
+    render() {
+        return ( 
+                <div>
+                    <New />
+                </div>
+               )
+    }
+}
