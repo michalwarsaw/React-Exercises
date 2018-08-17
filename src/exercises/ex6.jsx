a11173a3e11446fd84167d4e5d43b7d4a789fcee
Michal Wlodarczyk <polskito@gmail.com>
2018-08-17 13:01:58 +0200

    Update

--- src/exercises/ex6.jsx
+++ src/exercises/ex6.jsx
@@ -4,22 +4,21 @@ class One extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            object: {}
+            object: []
         }
    
     }
     // axios
         componentDidMount() {
-            axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => { this.setState({ object: response.data })})
-    }
-// https://jsonplaceholder.typicode.com/posts
-    render() {
-        return (
-                <div> 
-                    <h3> {this.state.object.title} </h3> 
-                </div>
-        )
+        }
+            render() {
+                return (
+                    <div> 
+                        <h3> {this.state.object.length && this.state.object[0].name} </h3> 
+                    </div>
+                       )
     }
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,5 @@ import App from './exercises/ex6.jsx';
 import './scss/style.scss';
 import 'whatwg-fetch';
 
+
 ReactDOM.render(<App />, document.getElementById('app'));
